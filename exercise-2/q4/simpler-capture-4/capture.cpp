2b48ac83b0f9eb733ced12fece7521b6d34bed1d
Roberto Baquerizo <roba8460@colorado.edu>
2022-06-22 15:53:49 -0600

    timestamp for average fps

--- exercise-2/q4/simpler-capture-4/capture.cpp
+++ exercise-2/q4/simpler-capture-4/capture.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *  Example by Sam Siewert 
+ *  Example by Sam Siewert
  *
  *  Created for OpenCV 4.x for Jetson Nano 2g, based upon
  *  https://docs.opencv.org/4.1.1
@@ -24,6 +24,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <time.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -35,37 +36,65 @@ using namespace std;
 #define ESCAPE_KEY (27)
 #define SYSTEM_ERROR (-1)
 
+double delta_t( struct timespec* stop, struct timespec* start )
+{
+    double current = ( (double)stop->tv_sec * 1000.0 ) +
+        ( (double)( (double)stop->tv_nsec / 1000000.0 ) );
+    double last = ( (double)start->tv_sec * 1000.0 ) +
+        ( (double)( (double)start->tv_nsec / 1000000.0 ) );
+    return ( current - last );
+}
+
 int main()
 {
-   VideoCapture cam0(0);
-   namedWindow("video_display");
-   char winInput;
+    // used to calculate total runtime
+    struct timespec mainStart = { 0, 0 };
+    struct timespec mainStop = { 0, 0 };
+    // used to calculate fps in while-loop
+    struct timespec start = { 0, 0 };
+    struct timespec stop = { 0, 0 };
+    double deltas = 0.0;
+    int framesProcessed = 0;
+    clock_gettime(CLOCK_REALTIME, &mainStart);
+    VideoCapture cam0( 0 );
+    namedWindow( "video_display" );
+    char winInput;
 
-   if (!cam0.isOpened())
-   {
-       exit(SYSTEM_ERROR);
-   }
+    if( !cam0.isOpened() )
+    {
+        exit( SYSTEM_ERROR );
+    }
 
-   cam0.set(CAP_PROP_FRAME_WIDTH, 640);
-   cam0.set(CAP_PROP_FRAME_HEIGHT, 480);
+    cam0.set( CAP_PROP_FRAME_WIDTH, 640 );
+    cam0.set( CAP_PROP_FRAME_HEIGHT, 480 );
 
-   while (1)
-   {
-      Mat frame;
-      cam0.read(frame);
-      imshow("video_display", frame);
+    while( 1 )
+    {
+        framesProcessed++;
+        clock_gettime( CLOCK_REALTIME, &start );
+        Mat frame;
+        cam0.read( frame );
+        imshow( "video_display", frame );
+        clock_gettime( CLOCK_REALTIME, &stop );
+        double tmp = delta_t( &stop, &start );
+        deltas += tmp;
 
-      if ((winInput = waitKey(10)) == ESCAPE_KEY)
-      //if ((winInput = waitKey(0)) == ESCAPE_KEY)
-      {
-          break;
-      }
-      else if(winInput == 'n')
-      {
-	  cout << "input " << winInput << " ignored" << endl;
-      }
-      
-   }
+        if( ( winInput = waitKey( 10 ) ) == ESCAPE_KEY )
+            //if ((winInput = waitKey(0)) == ESCAPE_KEY)
+        {
+            break;
+        }
+        else if( winInput == 'n' )
+        {
+            cout << "input " << winInput << " ignored" << endl;
+        }
 
-   destroyWindow("video_display"); 
+    }
+
+    destroyWindow( "video_display" );
+
+    double deltaT = deltas / framesProcessed;
+    deltaT = deltaT / 1000.0;
+    printf( "Average Frame Rate: %3.4f sec/frame\n\r", deltaT );
+    printf( "Average Frame Rate: %3.4f frames/sec (fps)\n\r", 1.0 / deltaT );
 };
