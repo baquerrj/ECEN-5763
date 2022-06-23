5588e573e6dc3422fd4e8691cc2e3ed796a65abb
Roberto Baquerizo <roba8460@colorado.edu>
2022-06-23 12:31:37 -0600

    upate help and create trackbars when creating window

--- exercise-2/q5/EdgeDetector.cpp
+++ exercise-2/q5/EdgeDetector.cpp
@@ -27,15 +27,17 @@ const int kernel_size = 3;
 
 const int maxKsize = 31;
 
-enum detector_e
-{
-    CANNY,
-    SOBEL,
-    NONE
-};
 
 class EdgeDetector
 {
+public:
+    enum detector_e
+    {
+        CANNY,
+        SOBEL,
+        NONE
+    };
+
 public:
     EdgeDetector( int deviceId = 0, int frameWidth = 640, int frameHeight = 480 )
     {
@@ -54,6 +56,10 @@ public:
         myScale = scale;
         myDelta = delta;
 
+        namedWindow( myWindowName );
+
+        createTrackbars();
+
     }
     ~EdgeDetector() {}
 
@@ -65,26 +71,24 @@ public:
     inline void setCurrentDetector( detector_e detection )
     {
         currentDetector = detection;
-        updateTrackbar();
     }
 
-    inline void updateTrackbar()
+    inline void showImage()
     {
-        if( currentDetector == CANNY )
-        {
-            createTrackbar( "Min Threshold:", myWindowName, &lowThreshold, max_lowThreshold, updateThreshold, this );
-        }
-        else if( currentDetector == SOBEL )
-        {
-            createTrackbar( "ksize", myWindowName, &ksize, maxKsize, updateKsize, this );
-            createTrackbar( "scale", myWindowName, &scale, 100, updateScale, this );
-            createTrackbar( "delta", myWindowName, &delta, 100, updateDelta, this );
-        }
+        imshow( myWindowName, myImageToShow );
+    }
+
+    inline void createTrackbars()
+    {
+        createTrackbar( "Canny Threshold:", myWindowName, &lowThreshold, max_lowThreshold, updateThreshold, this );
+        createTrackbar( "Sobel Kernel Size", myWindowName, &ksize, maxKsize, updateKsize, this );
+        createTrackbar( "Sobel Scale", myWindowName, &scale, 100, updateScale, this );
+        createTrackbar( "Sobel Delta", myWindowName, &delta, 100, updateDelta, this );
     }
 
     inline static void updateThreshold( int newValue, void* object )
     {
-        EdgeDetector* ed = (EdgeDetector*) object;
+        EdgeDetector* ed = (EdgeDetector*)object;
 
         ed->setCannyThreshold( newValue );
     }
@@ -159,19 +163,6 @@ public:
         }
     }
 
-    inline void showImage( bool original = true )
-    {
-        imshow( myWindowName, myImageToShow );
-        // if( original )
-        // {
-        //     imshow( myWindowName, mySource );
-        // }
-        // else
-        // {
-        //     imshow( myWindowName, myDestination );
-        // }
-    }
-
     inline int openImage( String name )
     {
         myWindowName = name;
@@ -201,56 +192,34 @@ private:
     int myDelta;
 };
 
-
-// static void CannyThreshold( int, void* )
-// void applyCanny( Mat* src, Mat* myDestination )
 void EdgeDetector::applyCanny()
 {
     Mat src_gray;
     cvtColor( mySource, src_gray, COLOR_BGR2GRAY );
 
-    //![reduce_noise]
     /// Reduce noise with a kernel 3x3
     blur( src_gray, detected_edges, Size( 3, 3 ) );
-    //![reduce_noise]
 
-    //![canny]
     /// Canny detector
-    printf( "myThreshold: %d\n", myThreshold );
     Canny( detected_edges, detected_edges, myThreshold, myThreshold * cannyRatio, kernel_size );
-    //![canny]
 
     /// Using Canny's output as a mask, we display our result
-    //![fill]
     myDestination = Scalar::all( 0 );
-    //![fill]
 
-    //![copyto]
     mySource.copyTo( myDestination, detected_edges );
-    //![copyto]
-
-    //![display]
-    // imshow( windowName, myDestination );
-    //![display]
 }
 
-// void applySobel( Mat* image, Mat* grad )
 void EdgeDetector::applySobel()
 {
-    // Mat src;
     Mat src_gray;
     Mat gray;
-    //![reduce_noise]
+
     // Remove noise by blurring with a Gaussian filter ( kernel size = 3 )
     GaussianBlur( mySource, gray, Size( 3, 3 ), 0, 0, BORDER_DEFAULT );
-    //![reduce_noise]
 
-    //![convert_to_gray]
     // Convert the image to grayscale
     cvtColor( mySource, src_gray, COLOR_BGR2GRAY );
-    //![convert_to_gray]
 
-    //![sobel]
     /// Generate grad_x and grad_y
     Mat grad_x, grad_y;
     Mat abs_grad_x, abs_grad_y;
@@ -260,15 +229,11 @@ void EdgeDetector::applySobel()
 
     /// Gradient Y
     Sobel( src_gray, grad_y, ddepth, 0, 1, myKsize, myScale, myDelta, BORDER_DEFAULT );
-    //![sobel]
 
-    //![convert]
     // converting back to CV_8U
     convertScaleAbs( grad_x, abs_grad_x );
     convertScaleAbs( grad_y, abs_grad_y );
-    //![convert]
 
-    //![blend]
     /// Total Gradient (approximate)
     addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, myDestination );
 }
@@ -276,43 +241,30 @@ void EdgeDetector::applySobel()
 int main( int argc, char** argv )
 {
     cv::CommandLineParser parser( argc, argv,
-                                  //   "{@input   |../data/lena.jpg|input image}"
                                   "{help    h|false|show help message}" );
     bool help = parser.get<bool>( "help" );
     if( help )
     {
-        cout << "The sample uses Sobel or Scharr OpenCV functions for edge detection\n\n";
+        cout << "The sample uses Sobel or Canny OpenCV functions for edge detection\n\n";
         parser.printMessage();
-        cout << "\nPress 'ESC' to exit program.\nPress 'R' to reset values ( ksize will be -1 equal to Scharr function )";
+        cout << "Press 'ESC' to exit program.\nPress 'c' to apply Canny edge detection\n";
+        cout << "Press 's' to apply Sobel edge detection\n";
+        cout << "Press 'n' to disable edge detection\n";
         return 0;
     }
 
-    EdgeDetector edgeDetector( 0, 640, 480 );
+    EdgeDetector * edgeDetector = new EdgeDetector();
 
-    // if( true == parser.has( "@input" ) )
-    // {
-    //     String imageName = parser.get<String>( "@input" );
-    //     int ret = edgeDetector.openImage( imageName );
-    //     if( ret == 1 )
-    //     {
-    //         return 1;
-    //     }
-    // }
-    // else
-    // {
-    edgeDetector.readCameraFrame();
-    // }
-    edgeDetector.applyTransformation();
-
-    edgeDetector.showImage( true );
+    edgeDetector->readCameraFrame();
+    edgeDetector->applyTransformation();
+    edgeDetector->showImage();
 
     while( true )
     {
-        edgeDetector.readCameraFrame();
-        edgeDetector.applyTransformation();
-        edgeDetector.showImage();
+        edgeDetector->readCameraFrame();
+        edgeDetector->applyTransformation();
+        edgeDetector->showImage();
 
-        // applySobel( &src, &myDestination );
         char key = (char)waitKey( 1 );
 
         if( key == 27 )
@@ -321,21 +273,21 @@ int main( int argc, char** argv )
         }
         else if( key == 'C' || key == 'c' )
         {
-            edgeDetector.setCurrentDetector( CANNY );
-            // edgeDetector.applyCanny();
-            // edgeDetector.showImage( false );
+            edgeDetector->setCurrentDetector( EdgeDetector::CANNY );
         }
         else if( key == 'S' || key == 's' )
         {
-            edgeDetector.setCurrentDetector( SOBEL );
-            // edgeDetector.applySobel();
-            // edgeDetector.showImage( false );
+            edgeDetector->setCurrentDetector( EdgeDetector::SOBEL );
         }
         else if( key == 'N' || key == 'n' )
         {
-            edgeDetector.setCurrentDetector( NONE );
-            // edgeDetector.showImage( true );
+            edgeDetector->setCurrentDetector( EdgeDetector::NONE );
         }
+    }
+
+    if( edgeDetector )
+    {
+        delete edgeDetector;
     }
     return 0;
 }
