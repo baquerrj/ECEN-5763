a62e6833b5007a8cb91e72707a6e8e664e9686e0
Roberto Baquerizo <roba8460@colorado.edu>
2022-06-30 21:10:58 -0600

    function for houghcircles

--- exercise-3/q3/houghcircles.cpp
+++ exercise-3/q3/houghcircles.cpp
@@ -9,6 +9,40 @@
 using namespace cv;
 using namespace std;
 
+void applyHoughCircles( Mat * src )
+{
+    //![convert_to_gray]
+    Mat gray;
+    cvtColor( *src, gray, COLOR_BGR2GRAY );
+    //![convert_to_gray]
+
+    //![reduce_noise]
+    medianBlur( gray, gray, 5 );
+    //![reduce_noise]
+
+    //![houghcircles]
+    vector<Vec3f> circles;
+    HoughCircles( gray, circles, HOUGH_GRADIENT, 1,
+                  gray.rows / 16,  // change this value to detect circles with different distances to each other
+                  100, 30, 1, 30 // change the last two parameters
+                  // (min_radius & max_radius) to detect larger circles
+    );
+    //![houghcircles]
+
+    //![draw]
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+        Vec3i c = circles[i];
+        Point center = Point( c[0], c[1] );
+        // circle center
+        circle( *src, center, 1, Scalar( 0, 100, 100 ), 3, LINE_AA );
+        // circle outline
+        int radius = c[2];
+        circle( *src, center, radius, Scalar( 255, 0, 255 ), 3, LINE_AA );
+    }
+    //![draw]
+}
+
 int main(int argc, char** argv)
 {
     //![load]
@@ -25,37 +59,7 @@ int main(int argc, char** argv)
     }
     //![load]
 
-    //![convert_to_gray]
-    Mat gray;
-    cvtColor(src, gray, COLOR_BGR2GRAY);
-    //![convert_to_gray]
-
-    //![reduce_noise]
-    medianBlur(gray, gray, 5);
-    //![reduce_noise]
-
-    //![houghcircles]
-    vector<Vec3f> circles;
-    HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
-                 gray.rows/16,  // change this value to detect circles with different distances to each other
-                 100, 30, 1, 30 // change the last two parameters
-            // (min_radius & max_radius) to detect larger circles
-    );
-    //![houghcircles]
-
-    //![draw]
-    for( size_t i = 0; i < circles.size(); i++ )
-    {
-        Vec3i c = circles[i];
-        Point center = Point(c[0], c[1]);
-        // circle center
-        circle( src, center, 1, Scalar(0,100,100), 3, LINE_AA);
-        // circle outline
-        int radius = c[2];
-        circle( src, center, radius, Scalar(255,0,255), 3, LINE_AA);
-    }
-    //![draw]
-
+    applyHoughCircles( &src );
     //![display]
     imshow("detected circles", src);
     waitKey();
