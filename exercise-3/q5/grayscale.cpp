2225a57bce9f1d36ede8d9722d43c6622526ce6e
Roberto Baquerizo <roba8460@colorado.edu>
2022-07-01 10:15:24 -0600

    q4

---
+++ exercise-3/q5/grayscale.cpp
@@ -0,0 +1,83 @@
+/**
+ * @file grayscale.cpp
+ * @brief This program performs background elimination and preserves the moving laser dot
+ */
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <opencv2/videoio.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+int main( int argc, char** argv )
+{
+
+    CommandLineParser parser( argc, argv,
+                              "{@input | ../Dark-Room-Laser-Spot-with-Clutter.mpeg | input video}" );
+
+    VideoCapture capture( parser.get<String>( "@input") );
+
+    Size frameSize = Size( (int)capture.get( CAP_PROP_FRAME_WIDTH ), (int)capture.get( CAP_PROP_FRAME_HEIGHT ) );
+
+    VideoWriter video;
+    video.open( "processed.mp4", VideoWriter::fourcc( 'M', 'P', '4', 'V' ),
+                capture.get( CAP_PROP_FPS ), frameSize, true );
+
+    if( !video.isOpened() )
+    {
+        printf( "Could not open output video stream!\n\r" );
+        exit( -1 );
+    }
+
+    if( !capture.isOpened() )
+    {
+        printf( "Coult not open input video stream!\n\r" );
+        exit( -1 );
+    }
+
+    Mat frame;
+    capture.read( frame );
+    int framesProcessed = 0;
+
+    int numberOfChannels = frame.channels();
+    Mat frameBGR[numberOfChannels];
+
+    char winInput;
+
+    String currentFrame = "Current Frame";
+
+    namedWindow( currentFrame, WINDOW_GUI_NORMAL );
+
+    while( true )
+    {
+        capture.read( frame );
+
+        if( frame.empty() )
+        {
+            break;
+        }
+
+        split( frame, frameBGR );
+
+        framesProcessed++;
+
+        video.write( frameBGR[1] );
+
+        imshow( currentFrame, frameBGR[1] );
+
+        winInput = waitKey( 2 );
+        if( 27 == winInput )
+        {
+            break;
+        }
+    }
+
+    capture.release();
+    video.release();
+
+    destroyWindow( currentFrame );
+
+    return 0;
+}
